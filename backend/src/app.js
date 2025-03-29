6864adc1b8189eccd5f914f350058120056dd80a
Charyll <charllycbncl@gmail.com>
2025-03-29 18:30:59 +0800

    email API integrated and report generation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,8 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import emailRoutes from "./routes/emailRoutes.js";
 import "./config/firebase.js"; // Initialize Firebase
-
-dotenv.config();
 
 const app = express();
 
@@ -16,7 +13,4 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/email", emailRoutes); // Added Email Routes
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+export default app;
