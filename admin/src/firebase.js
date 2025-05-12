87650af720c4ba9f1dfbaa7285565f8b8a514ffd
Charyll <charllycbncl@gmail.com>
2025-05-13 02:02:10 +0800

    working client & admin portal

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -1,23 +1,17 @@
-// Import necessary Firebase SDKs
+// firebase.ts
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; // Import getAuth to manage authentication
+import { getAuth } from "firebase/auth";
 
-// Your Firebase configuration object (replace with your actual Firebase config)
 const firebaseConfig = {
-  apiKey: "AIzaSyC-Fh1dfuXRw3tm85G4DGUTdHuM023Ylsk",
-  authDomain: "sonar-2025.firebaseapp.com",
-  projectId: "sonar-2025",
-  storageBucket: "sonar-2025.firebasestorage.app",
-  messagingSenderId: "934904778488",
-  appId: "1:934904778488:web:b00752ef24e5cb1a4af50d",
-  measurementId: "G-08C8HLY9ZJ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Auth
-export const auth = getAuth(app); // This exports the 'auth' object that you can use in Login.jsx
-
-// You can also export the app if needed (optional)
+export const auth = getAuth(app);
 export { app };
